2f202869b0fb9e5da88a5bbfe253ed9a7fbc5faa
Anton Vouk <avo94eu@gmail.com>
2023-06-08 07:55:22 -0400

    Initial commit

--- src/main/java/Algorithms/Prof1Homework_02_06_2023/StringApp.java
+++ src/main/java/Algorithms/Prof1Homework_02_06_2023/StringApp.java
@@ -1,21 +1,35 @@
 package Algorithms.Prof1Homework_02_06_2023;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringApp {
 
-    static List<String> strings;
-
     public static void main(String[] args) {
-        modifier("candibober").forEach(System.out::println);
+        System.out.println(modifier("cafe"));
     }
 
     public static List<String> modifier(String original) {
-        recursion(original.toCharArray(), new char[0]);
+        List<String> strings = new ArrayList<>();
+        recursion(original.toCharArray(), 0, strings);
         return strings;
     }
 
-    private static void recursion(char[] string, char[] memory) {
+    private static void recursion(char[] string, int startIndex, List<String> strings) {
+        if (startIndex == string.length - 1) {
+            strings.add(new String(string));
+        } else {
+            for (int i = startIndex; i < string.length; i++) {
+                swap(string, startIndex, i);
+                recursion(string, startIndex + 1, strings);
+                swap(string, startIndex, i);
+            }
+        }
+    }
 
+    private static void swap(char[] array, int i, int j) {
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
